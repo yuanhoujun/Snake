87c442052988ee4ff1ea16713c29f4265ba13fae
Scott Smith <ouyangfeng2016@gmail.com>
2018-01-05 15:58:44 +0800

    Activity滑动释放Window属性还原，避免因为设置透明属性导致Activity生命周期出现异常

--- snake/src/main/java/com/youngfeng/snake/Snake.java
+++ snake/src/main/java/com/youngfeng/snake/Snake.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -415,6 +416,7 @@ public class Snake {
                             if(null != viewOfLastActivity) {
                                 viewOfLastActivity.setLeft(0);
                             }
+                            ActivityHelper.convertFromTranslucent(activity);
                         }
                     });
                 }
