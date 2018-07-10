6db80ddebc693a3079439e2cd5fa544f96b07aa1
欧阳锋 <ouyangfeng2016@gmail.com>
2018-07-10 15:45:08 +0800

    Add if debug to work

--- snake/src/main/java/com/youngfeng/snake/util/Logger.java
+++ snake/src/main/java/com/youngfeng/snake/util/Logger.java
@@ -9,17 +9,17 @@ import android.util.Log;
  * @author Scott Smith 2018-01-16 13:33
  */
 public class Logger {
-    public static boolean debug = false;
+    public static boolean debug = true;
     public static final String TAG = "Snake";
 
     public static void d(String msg) {
-        if(!TextUtils.isEmpty(msg)) {
+        if(debug && !TextUtils.isEmpty(msg)) {
             Log.d(TAG, msg);
         }
     }
 
     public static void e(String msg) {
-        if(!TextUtils.isEmpty(msg)) {
+        if(debug && !TextUtils.isEmpty(msg)) {
             Log.e(TAG, msg);
         }
     }
